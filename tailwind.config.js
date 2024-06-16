a68d7e8bb2aa2308d287d6862b4962c13657510d
!abdu11ah <muhammadabdullah8040@gmail.com>
2024-06-16 16:06:25 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,7 @@ module.exports = {
   ],
   darkMode: ["class"],
   theme: {
-    extend: {
-      colors: {
-        'code-bg': '#f5f5f5',  // Light gray background for code blocks
-        'code-text': '#333',   // Dark text color for code blocks
-        'inline-code-bg': '#e0e0e0', // Light gray background for inline code
-      },
-      fontFamily: {
-        'code': ['Source Code Pro', 'monospace'],
-      },
-    },
+    extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
-};
+}
