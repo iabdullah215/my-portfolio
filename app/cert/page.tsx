69442c911854bc7a695b2f9eb5de471f42f49750
!abdu11ah <muhammadabdullah8040@gmail.com>
2024-08-08 15:44:59 +0500

    Update page.tsx

--- app/cert/page.tsx
+++ app/cert/page.tsx
@@ -4,7 +4,7 @@ import Cert from "@/components/Cert";
 export default function CertPage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-8">My Certifications</h1>
+      <h1 className="text-3xl font-extrabold mb-8 mt-12">My Certifications</h1>
       <Cert /> {/* This will render the certifications with images and descriptions */}
     </div>
   );
