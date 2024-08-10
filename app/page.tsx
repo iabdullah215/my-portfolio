323acc5359843836e8b37e2addc979796362c6f6
!abdu11ah <muhammadabdullah8040@gmail.com>
2024-08-10 17:49:33 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
   return (
     <div className="prose dark:prose-invert">
       {sortedPosts.map((post, index) => (
-        <div key={post._id}>
-          <article className="grid grid-cols-3 gap-6 py-4">
+        <div key={post._id} className="pt-6">
+          <article className="grid grid-cols-3 gap-6 items-center py-4">
             <div className="text-gray-500 dark:text-gray-400 col-span-1">
               {new Date(post.date).toLocaleDateString('en-US', {
                 year: 'numeric',
