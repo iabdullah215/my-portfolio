19ea1e125e0ade50d3e316160a87d383200ef7c7
!abdu11ah <muhammadabdullah8040@gmail.com>
2024-08-10 18:11:29 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold mt-4 mb-2 pt-6">Blogs</h1>
       {/* Description below the heading */}
       <p className="text-lg text-gray-600 dark:text-gray-400 mb-6">
-        Welcome to my blog! Here you&apos;ll find a collection of my latest posts on various topics. Enjoy reading!
+        Collection of my thoughts on various topics
       </p>
       {/* Search bar */}
       <div className="mb-4">
