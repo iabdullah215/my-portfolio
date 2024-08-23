9d1c22e3b2b5d816669525770ad27d8668a0296d
!abdu11ah <muhammadabdullah8040@gmail.com>
2024-08-24 01:03:10 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                   <Link href="/">Blogs</Link>
                   <Link href="/about">About</Link>
                   <Link href="/cert">Certifications</Link>
-                  <Link href="/profile"></Profile>
+                  <Link href="/profile">Profile</Link>
                 </nav>
               </div>
             </header>
