9dde4c8c7276538a7aa68823f5ecbb2c7afdd36f
!abdu11ah <muhammadabdullah8040@gmail.com>
2024-08-24 00:03:09 +0500

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 const ProfilePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-gray-900 pt-10 pb-8">
-      <div className="relative w-40 h-40 mb-6"> 
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-gray-900">
+      <div className="relative w-40 h-40 mb-6">
         <Image
           src="/static/images/mr.r0b0t.jpg"
           alt="Profile Picture"
@@ -14,8 +14,8 @@ const ProfilePage = () => {
       <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100 mb-2">
         !abdu11ah
       </h1>
-      <p className="text-lg text-gray-600 dark:text-gray-300">
-        From the shadows, I control.
+      <p className="text-lg text-gray-600 dark:text-gray-300 text-center">
+        Your description goes here. You can add any information about yourself or your work.
       </p>
     </div>
   );
