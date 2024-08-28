69176360e211082215886b19c289e1c132ccd42d
!abdu11ah <muhammadabdullah8040@gmail.com>
2024-08-28 14:53:08 +0500

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,32 +1,13 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider defaultTheme="light" {...props}>
+    <NextThemesProvider defaultTheme="light" {...props} enableSystem={false}>
       {children}
     </NextThemesProvider>
   )
 }
-
-const ThemedComponent = () => {
-  const { theme } = useTheme()
-
-  return (
-    <div
-      style={{
-        backgroundColor: theme === "dark" ? "#3a3b3c" : "#f7fafc",
-        color: theme === "dark" ? "#f7fafc" : "#1a202c",
-        padding: "20px",
-        borderRadius: "8px",
-      }}
-    >
-      This is some themed content with a custom dark background color.
-    </div>
-  )
-}
-
-export default ThemedComponent
