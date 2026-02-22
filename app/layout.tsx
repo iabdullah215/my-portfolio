55138d680a37596ab6cbc9dccafe9d386378a34f
iabdullah215 <muhammadabdullah8040@gmail.com>
2026-02-22 12:02:31 +0500

    Update root

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
               <div className="flex items-center justify-between">
                 {/* Removed ModeToggle */}
                 <nav className="ml-auto text-sm font-medium space-x-6">
-                  <Link href="/">Blogs</Link>
+                  <Link href="/blog">Blogs</Link>
                   <Link href="/about">About</Link>
                   <Link href="/cert">Certifications</Link>
-                  <Link href="/profile">Profile</Link>
+                  <Link href="/">Profile</Link>
                 </nav>
               </div>
             </header>
